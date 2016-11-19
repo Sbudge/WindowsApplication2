6559e3e4bde22a9f022a85a1fceb9987910efffc
Shari Budge <Shari Budge@SILVER>
2016-11-19 11:49:29 -0500

    Add project files.

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Process.Start("C:\Program Files (x86)\Readiris Pro 15\readiris.exe")
+    End Sub
+End Class
